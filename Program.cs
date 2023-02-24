3c3e32aebd18f9ccc833d27faec55c60865a993d
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-24 19:33:17 +0530

    Create simple linked list

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LinkedListProblems
+﻿using System.Collections.Generic;
+
+namespace LinkedListProblems
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked list problems");
+            //Console.WriteLine("Welcome to Linked list problems");
+
+            LinkedList linked = new LinkedList();
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(70);
+            
+            linked.Display();
         }
     }
 }
